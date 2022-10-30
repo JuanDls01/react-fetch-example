c1f0c7e5977a9d111ce616bbe0e78061c4c44c46
JuanDls01 <juanignaciodelossantos01@gmail.com>
2022-10-30 17:48:51 -0300

    fix/ navigate to home replace true

--- src/App.tsx
+++ src/App.tsx
@@ -18,8 +18,8 @@ function App() {
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
           <Route path='/' element={<Navigate to={"home"} replace={true} />} />
-          <Route path='/home' element={<Home />} />
-          <Route path='/create-product' element={<CreateProduct />} />
+          <Route path='home' element={<Home />} />
+          <Route path='screate-product' element={<CreateProduct />} />
           <Route path='*' element={<>PAGE NOT FOUNDED</>} />
         </Routes>
       </Suspense>
