6d45a1f2a191d709c5b7a70aa71f985ba27090c5
JuanDls01 <juanignaciodelossantos01@gmail.com>
2022-10-30 14:11:14 -0300

    feat/ add route page not founded and navigate to home on default route

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Suspense, lazy } from "react";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import { Navbar } from "./components";
 import { useThemeContext } from "./Context/ThemeContext";
 // import { CreateProduct, Home } from "./pages";
@@ -17,8 +17,10 @@ function App() {
       <Navbar />
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
+          <Route path='/' element={<Navigate to={"home"} />} />
           <Route path='/home' element={<Home />} />
           <Route path='/create-product' element={<CreateProduct />} />
+          <Route path='*' element={<>PAGE NOT FOUNDED</>} />
         </Routes>
       </Suspense>
     </div>
